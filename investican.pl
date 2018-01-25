13b710a5a401f250139043e6adb5d4e27d4aee82
rockwolf <thereisanewway@gmail.com>
2018-01-25 13:27:54 +0100

    Better comments, trying to get it to work correctly.

--- investican.pl
+++ investican.pl
@@ -1,10 +1,10 @@
-/*
- * Investican.pl:
- * Check commodities, to see if they can be possible investment candidates,
- * based on specific criteria.
- *
- * See LICENSE.txt for license info.
- */
+%
+% Investican.pl:
+% Check commodities, to see if they can be possible investment candidates,
+% based on specific criteria.
+%
+% See LICENSE.txt for license info.
+%
 
 /* Needs:
   - to be a stock or a fund
@@ -15,9 +15,11 @@
   - or a double tax that is < 20%
 */
 
-/* Facts */
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Facts
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-/** Market information **/
+%%% Market information
 market(etr).
 market(ebr).
 market(ams).
@@ -25,26 +27,16 @@ market(ams).
 has_low_tax(ebr).
 has_low_tax(ams).
 
-/** Stocks **/
-/*** etr ***/
-stock(fme).
-/*** ebr ***/
-stock(cofb).
-stock(tess).
-stock(tnet).
-stock(exm).
-/*** ams ***/
-stock(sbm).
-
-/*** etr ***/
-market_of_stock(fme, etr).
-/*** ebr ***/
-market_of_stock(cofb, ebr).
-market_of_stock(tess, ebr).
-market_of_stock(tnet, ebr).
-market_of_stock(exm, ebr).
-/*** ams ***/
-market_of_stock(sbm, ams).
+%%% Stocks
+% etr
+stock(fme, etr).
+%% ebr
+stock(cofb, ebr).
+stock(tess, ebr).
+stock(tnet, ebr).
+stock(exm, ebr).
+% ams
+stock(sbm, ams).
 
 stock_10y_low(fme, 1).
 stock_10y_low(cofb, 1).
@@ -57,24 +49,19 @@ stock_10y_high(tess, 10).
 stock_10y_high(tnet, 10).
 stock_10y_high(sbm, 10).
 
-/** Funds **/
-/*fund(ebr, tst).*/
+%%% Funds
+%fund(ebr, tst).
 
-/* Predicates */
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Predicates
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-has_potential(Market, Stock) :-
-  stock(Stock),
-  market_of_stock(Stock, Market),
+has_potential(Code, Market) :-
+  stock(Code, Market),
   has_low_tax(Market).
 
 find_all(Market) :-
-  forall(Market, write_all(Market)). 
-
-write_all(Market) :-
   open('result.txt', write, Stream),
-  setof(X, (has_potential(Market, X)), X0),
-  write(Stream, Market),
-  write(Stream, '.'),
-  write(Stream, X0),
-  write(Stream, nl),
+  setof(X, (has_potential(X, Market)), X0), 
+  forall(member(X, X0), write(Stream, X)),
   close(Stream).
