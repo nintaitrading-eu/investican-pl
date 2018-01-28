29ecd8d7dd7fbe7e13644f87896e234399e45ac8
rockwolf <thereisanewway@gmail.com>
2018-01-28 21:39:22 +0100

    Checking in test-changes.

--- investican.pl
+++ investican.pl
@@ -22,7 +22,7 @@
 :- use_module(library(csv)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Init 
+% Init
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 :- set_prolog_flag(verbose, silent).
 :- initialization main.
@@ -30,7 +30,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Facts
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Loaded from csv-files
+% Note: Changeable datasets for filling the knowledge
+% base, are loaded from csv-files.
 
 %%% Market information
 % TODO: move to csv file.
@@ -53,7 +54,7 @@ import_facts :-
   csv_read_file('stocks.csv', Stocks, [functor(stock), separator(0';)]),
   maplist(writeln, Stocks),
   maplist(assert, Stocks),
-  % markets 
+  % markets
   write("Loading markets from markets.csv..."), nl,
   csv_read_file('markets.csv', Markets, [functor(market), separator(0';)]),
   maplist(writeln, Markets),
@@ -63,15 +64,37 @@ has_potential(Code, Market) :-
   stock(Code, Market),
   market(Market, 'low_tax').
 
+convert_to_json(Market, Code, Result) :-
+  Result is format("{\"market\": \"~w\",\"commodity\": \"~w\"}", [Market, Code]).
+
 %dash_to_dots(AnElement, ConvertedAtom) :-
 %  atomic_list_concat(Words, '-', AnElement),
 %  atomic_list_concat(Words, '.', ConvertedAtom).
 
+%make a list that changes
+%test([], NewList).
+test([A, B|T], NewList) :-
+  write('test: A='), write(A), nl,
+  write('test: B='), write(B), nl,
+  write('test: T='), write(T), nl,
+  % TODO: Arguments not sufficiently instantiated.
+  % TODO: Finish function and extend it to make [A,B] jsonified.
+  append([{"lala":A, "lblb":B}], NewList),
+  write('test: NewList after append='), write(NewList), nl.
+  %test(T, NewList).
+
 main :-
   import_facts,
   %dash_to_dots('market-stock', ConvertedAtom),
-  setof({"\"market\"": Y, "\"commodity\"": X}, (has_potential(X, Y)), X0),
-  open('result.txt', write, Stream),
-  %write(X0), nl.
-  write(Stream, X0),
-  close(Stream).
+  % TODO: The call to convert... does not work. It gets added to the txt file.
+  %convert_to_json(X, Y, R),
+  setof([Y, X], (has_potential(X, Y)), X0),
+  % TODO: make Choco be:
+  % [{"market": "Y0", "code": "X0"}, ...]
+  test(X0, Choco),
+  write(Choco), nl,
+  %write(X0), nl,
+  %open('result.txt', write, Stream),
+  %write(Stream, R),
+  %close(Stream),
+  write('Done.'), nl.
